94c1dff17c4a004b16f6009f62c6e9f3782783f4
Leen-massarwy <84320246+Leen-massarwy@users.noreply.github.com>
2025-01-14 17:06:14 +0200

    create table component

--- Factory/src/App.jsx
+++ Factory/src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import Employess from "./Pages/Employees/Employess";
 
 const App = () => {
   return (
     <>
       <h1>Vite + React</h1>
+      <Employess />
     </>
   );
 };
---
+++ Factory/src/Components/Table.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+const Table = (props) => {
+  return (
+    <table border="1">
+      <thead>
+        {props.columns.map((e) => {
+          return (
+            <th
+              key={e._id}
+              style={{
+                width: `${100 / props.columns.length}%`,
+              }}
+            >
+              {e.title}
+            </th>
+          );
+        })}
+      </thead>
+      <tbody>
+        {props.source.map((data, index) => {
+          return (
+            <tr key={data._id}>
+              {props.columns.map((e) => {
+                return (
+                  <td
+                    key={e._id}
+                    style={{
+                      width: `${100 / props.columns.length}%`,
+                    }}
+                  >
+                    {data[e.dataIndex]}
+                  </td>
+                );
+              })}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
+
+export default Table;
