ebf0331746a93300e8c1990ac3abf50bea3dc3f4
Leen-massarwy <115391076+LeenMass@users.noreply.github.com>
2025-01-15 16:26:21 +0200

    creating departments drop down

---
+++ Factory/src/Pages/Employees/AddEmployee.jsx
@@ -0,0 +1,55 @@
+import axios from "axios";
+import React, { useState } from "react";
+const EmployeesUrl = "http://localhost:4000/employees";
+
+const AddEmployee = () => {
+  const [newEmployee, setNewEmployee] = useState({
+    first_name: "",
+    last_name: "",
+    start_work_year: "",
+    department_id: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setNewEmployee({ ...newEmployee, [name]: value });
+  };
+  const saveEmployee = async () => {
+    try {
+      console.log(newEmployee);
+      const { data } = await axios.post(EmployeesUrl, newEmployee);
+      console.log(data);
+      return data;
+    } catch (error) {
+      console.error("Error saving employee:", error);
+    }
+  };
+
+  return (
+    <div>
+      AddEmployee
+      <input
+        name="first_name"
+        type="text"
+        placeholder="enter employee's first_name"
+        onChange={handleChange}
+      />
+      <input
+        name="last_name"
+        type="text"
+        placeholder="enter employee's last_name"
+        onChange={handleChange}
+      />
+      <input
+        name="start_work_year"
+        type="number"
+        placeholder="start_year"
+        onChange={handleChange}
+      />
+      <DepartmentsDropdown />
+      <button onClick={saveEmployee}>Save</button>
+    </div>
+  );
+};
+
+export default AddEmployee;
